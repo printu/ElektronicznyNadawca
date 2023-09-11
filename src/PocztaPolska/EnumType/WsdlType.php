ad46f290af39d6d7b0d168da8e608c28245568ae
Paweł Krzaczkowski <pawel@freshmind.pl>
2023-09-11 11:01:16 +0200

    chore: fix

---
+++ src/PocztaPolska/EnumType/WsdlType.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PocztaPolska\EnumType;
+
+/**
+ * @subpackage Enumerations
+ */
+class WsdlType
+{
+    const WSDL_FILE = 'wsdl/en.wsdl';
+    const WSDL_TEST_FILE = 'wsdl/test/en.wsdl';
+}
