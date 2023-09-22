087d6ba80f8d2d7d8518936e75ea94d5f66fd9e3
Paweł Krzaczkowski <pawel@freshmind.pl>
2023-09-22 16:26:13 +0200

    fix: add getPath() method to retrieve wsdl file path

--- src/PocztaPolska/EnumType/WsdlType.php
+++ src/PocztaPolska/EnumType/WsdlType.php
@@ -11,4 +11,13 @@ class WsdlType
 {
     public const WSDL_FILE = '../../../wsdl/en.wsdl';
     public const WSDL_TEST_FILE = '../../../wsdl/test/en.wsdl';
+
+    public static function getPath(bool $sandbox = false): string
+    {
+        if ($sandbox) {
+            return realpath(__DIR__ . '/' . self::WSDL_TEST_FILE);
+        }
+
+        return realpath(__DIR__ . '/' . self::WSDL_FILE);
+    }
 }
